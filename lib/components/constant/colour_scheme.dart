1ec29aedcabc95d358cd34b9c1578014204b6595
KwesiEren <coderoidgh10@gmail.com>
2025-03-15 23:53:39 +0000

    Created the asset files for Textstyle and Colors

---
+++ lib/components/constant/colour_scheme.dart
@@ -0,0 +1,16 @@
+// Application theme controller
+import 'package:flutter/material.dart';
+
+// Components and Background colors
+const base_color = Colors.white;
+const main_color = Colors.red;
+const card_color = Colors.amber;
+
+// Button colors
+const buton_color1 = Colors.pinkAccent;
+const buton_color2 = Colors.yellow;
+
+// Text colors
+const text_color1 = Colors.black;
+const text_color2 = Colors.grey;
+const text_color3 = Colors.blueAccent;
---
+++ lib/components/constant/text_styles.dart
@@ -0,0 +1,13 @@
+// Text Style controlers for the project
+
+import 'package:flutter/material.dart';
+import 'package:stylish_ecommerce_app/components/constant/colour_scheme.dart';
+
+const kheading1 =
+    TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: text_color3);
+
+const kheading2 =
+    TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: text_color1);
+
+const kheading3 =
+    TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: text_color2);
