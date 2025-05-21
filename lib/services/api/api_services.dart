dcf4208627ff614eb5704cd70e5f3fb8d95da87f
KwesiEren <coderoidgh10@gmail.com>
2025-05-21 12:50:05 +0000

    update

--- lib/services/api/api_services.dart
+++ lib/services/api/api_services.dart
@@ -9,8 +9,8 @@ class ApiService {
     BaseOptions(
       baseUrl:
           "https://j85nv7qj-5000.uks1.devtunnels.ms/api", // Update with your ngrok URL
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 10),
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 30),
       headers: {
         'Content-Type': 'application/json',
       },
